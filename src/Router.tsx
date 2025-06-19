eec4c47a40f1813ad9a6006ebb552b6180d4e1fd
svyatoslavvarnitskyy <varnisvy000@gmail.com>
2025-06-19 15:41:22 -0500

    First commit

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
+import {ErrorPage} from "@/pages/Error.page.";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
   },
+  {
+    path: "/*",
+    element: <ErrorPage/>
+  }
 ]);
 
 export function Router() {
--- test-utils/render.tsx
+++ test-utils/render.tsx
@@ -1,11 +1,11 @@
 import { render as testingLibraryRender } from '@testing-library/react';
 import { MantineProvider } from '@mantine/core';
-import { theme } from '../src/theme';
+import { shadcnTheme } from '@/theme';
 
 export function render(ui: React.ReactNode) {
   return testingLibraryRender(ui, {
     wrapper: ({ children }: { children: React.ReactNode }) => (
-      <MantineProvider theme={theme} env="test">
+      <MantineProvider theme={shadcnTheme} env="test">
         {children}
       </MantineProvider>
     ),
