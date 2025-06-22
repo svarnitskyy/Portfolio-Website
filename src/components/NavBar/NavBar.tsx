3d2dcb5832801d59ae66c58830581aa793b9bf6e
svyatoslavvarnitskyy <varnisvy000@gmail.com>
2025-06-22 15:10:39 -0500

    NavBar responsive

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,28 +1,32 @@
-import {Button, Grid, Group, Image, Divider} from '@mantine/core';
+import { Button, Flex, Image, Divider, Box } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
+import './NavBar.css';
 
 export function Navbar() {
-  return (
-      <div style={{justifyItems: 'center', position: 'sticky', zIndex: 10000, width: '100%', top: '0px', backdropFilter: 'blur(5px)'}}>
-          <div style={{width:'100%'}}>
-              <Grid align='center'>
-                  <Grid.Col span="auto" style={{ textAlign: 'left', marginLeft: '100px' }}>
-                      <Button variant="subtle">
-                          <Image src="./src/favicon.svg" h={50} fit="contain" />
-                      </Button>
-                  </Grid.Col>
-                  <Grid.Col span={6}>
-                      <Group justify="center">
-                          <a href="#top"><Button variant="subtle">Home</Button></a>
-                          <Button variant="subtle">Experience</Button>
-                          <Button variant="subtle">Projects</Button>
-                      </Group>
-                  </Grid.Col>
-                  <Grid.Col span="auto" style={{ textAlign: 'right', marginRight: '100px' }}>
-                      <Button variant="subtle">Contact</Button>
-                  </Grid.Col>
-              </Grid>
-              <Divider />
-          </div>
-      </div>
-  );
+    const isMobile = useMediaQuery('(max-width: 768px)');
+    const buttonSize = isMobile ? 'xs' : 'md';
+
+    return (
+        <Box className="navbar">
+            <Flex className="navbar-inner" align="center" justify="space-between">
+                {!isMobile && (
+                    <Box className="logo">
+                        <Button variant="subtle">
+                            <Image src="./src/favicon.svg" h={40} fit="contain" />
+                        </Button>
+                    </Box>
+                )}
+
+                <Box className="nav-links-wrapper">
+                    <Flex className="nav-links" gap={isMobile ? 'xs' : 'sm'} justify="center" wrap="nowrap">
+                        <a href="#top"><Button variant="subtle" size={buttonSize}>Home</Button></a>
+                        <Button variant="subtle" size={buttonSize}>About</Button>
+                        <Button variant="subtle" size={buttonSize}>Projects</Button>
+                        <Button variant="subtle" size={buttonSize}>Contact</Button>
+                    </Flex>
+                </Box>
+            </Flex>
+            <Divider />
+        </Box>
+    );
 }
