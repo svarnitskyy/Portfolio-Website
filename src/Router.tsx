05761b4904b43ba811965c20fec9e856229ecd7b
svyatoslavvarnitskyy <varnisvy000@gmail.com>
2025-07-09 13:25:12 -0500

    Deploy

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,21 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ErrorPage } from '@/pages/Error.page.';
 import { HomePage } from './pages/Home.page';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/*',
+      element: <ErrorPage />,
+    },
+  ],
   {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/*',
-    element: <ErrorPage />,
-  },
-]);
+    basename: '/Portfolio-Website',
+  }
+);
 
 export function Router() {
   return <RouterProvider router={router} />;
